a16210150f5cc3b72fcbe566e290312a0bdcc272
glaucio.ricardo <glaucioripol@gmail.com>
2021-01-26 11:15:41 -0300

    feat: done calculate in calc service, and write tests

--- src/app/calc/services/calc.service.spec.ts
+++ src/app/calc/services/calc.service.spec.ts
@@ -10,7 +10,37 @@ describe('CalcService', () => {
     service = TestBed.inject(CalcService);
   });
 
-  it('should be created', () => {
-    expect(service).toBeTruthy();
+  describe('calculate', () => {
+    it('SUM should be return 10', () => {
+      const EXPECTED_VALUE = 10;
+      const INPUT_VALUE = 5;
+
+      expect(service.calculate(INPUT_VALUE, INPUT_VALUE, service.SUM)).toBe(EXPECTED_VALUE);
+    });
+
+    it('SUBTRACTION should be return 48', () => {
+      const EXPECTED_VALUE = 48;
+
+      expect(service.calculate(50, 2, service.SUBTRACTION)).toBe(EXPECTED_VALUE);
+    });
+
+    it('DIVISION should be return 25', () => {
+      const EXPECTED_VALUE = 25;
+
+      expect(service.calculate(50, 2, service.DIVISION)).toBe(EXPECTED_VALUE);
+    });
+
+    it('MULTIPLICATION should be return 100', () => {
+      const EXPECTED_VALUE = 100;
+
+      expect(service.calculate(50, 2, service.MULTIPLICATION)).toBe(EXPECTED_VALUE);
+    });
+
+    it('should be return 0', () => {
+      const EXPECTED_VALUE = 0;
+
+      expect(service.calculate(100, 1000, service.CLEAN_UP)).toBe(EXPECTED_VALUE);
+    });
   });
+
 });
--- src/app/calc/services/calc.service.ts
+++ src/app/calc/services/calc.service.ts
@@ -3,5 +3,24 @@ import { Injectable } from '@angular/core';
 @Injectable({ providedIn: 'root' })
 export class CalcService {
 
-  constructor() { }
+  readonly SUM = '+';
+  readonly SUBTRACTION = '-';
+  readonly DIVISION = '/';
+  readonly MULTIPLICATION = '*';
+  readonly CLEAN_UP = 'C';
+
+  calculate(firstNumber: number, secondNumber: number, operation?: string): number {
+    const calcSolvers = {
+      [this.SUM]: () => firstNumber + secondNumber,
+      [this.SUBTRACTION]: () => firstNumber - secondNumber,
+      [this.DIVISION]: () => firstNumber / secondNumber,
+      [this.MULTIPLICATION]: () => firstNumber * secondNumber,
+      [this.CLEAN_UP]: () => 0,
+    };
+
+    const applyOperation = calcSolvers[operation]();
+
+    return applyOperation;
+  }
+
 }
